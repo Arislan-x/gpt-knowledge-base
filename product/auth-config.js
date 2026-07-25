d1651d13a988d16594be28b9914a4aec7dfffeab
Arislan-x <songsida_x@163.com>
2026-07-26 04:20:52 +0800

    Restore public product site to 1.3.8

--- product/auth-config.js
+++
@@ -1,4 +0,0 @@
-window.GPTKB_AUTH_CONFIG = Object.freeze({
-  supabaseUrl: "https://uouolfdzpipiwjqxodmz.supabase.co",
-  publishableKey: "sb_publishable_SFcgkixFiv5Q63XKn2XEVA_uBj1x9oE"
-});
--- product/billing-config.js
+++
@@ -1,4 +0,0 @@
-window.GPTKB_BILLING_CONFIG = Object.freeze({
-  environment: "live",
-  clientToken: "live_90e4e1d90a25bad26d8b6197cb6"
-});
